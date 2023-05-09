702718b316b6777c0ba0fad01eefb0b13a40c0ab
Conrad Koziol <ckoziol@gmail.com>
2023-05-08 21:07:00 -0700

    Reduce k=3 to k=2

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -55,6 +55,6 @@ export const makeChain = (
     combineDocumentsChain: docChain,
     questionGeneratorChain: questionGenerator,
     returnSourceDocuments: true,
-    k: 3, //number of source documents to return
+    k: 2, //number of source documents to return
   });
 };
